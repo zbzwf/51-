ed8b9dab6488a3d428005003587681890526e1a3
zbzwf <97572249+zbzwf@users.noreply.github.com>
2022-02-03 11:00:00 +0800

    提交单片机代码

---
+++ 22-2-3.c
@@ -0,0 +1,41 @@
+#include <REGX52.H>
+
+void Delay500ms()		//@12.000MHz
+{
+	unsigned char i, j, k;
+
+
+	i = 4;
+	j = 205;
+	k = 187;
+	do
+	{
+		do
+		{
+			while (--k);
+		} while (--j);
+	} while (--i);
+}
+
+void main()
+{
+	while(1)
+		
+		{	P2=0XFE;
+			Delay500ms();
+			P2=0XFD;
+			Delay500ms();
+			P2=0XFB;
+			Delay500ms();
+			P2=0XF7;
+			Delay500ms();
+			P2=0XEF;
+			Delay500ms();
+			P2=0XDF;
+			Delay500ms();
+			P2=0XBF;
+			Delay500ms();
+			P2=0X7F;
+			Delay500ms();
+		}
+}
